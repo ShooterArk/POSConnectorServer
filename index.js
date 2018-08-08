098e402ef524ecd4e2b369204900f83bc3538198
ShooterArk <smallassasin@gmail.com>
2018-08-09 02:04:13 +0500

    Removed no-access-control

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ connections = [];
 
 app.use(express.json());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
